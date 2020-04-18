d8dcb0d1274fd09f24397f9386726a8fd8176d3e
Martin Vilcans <martin@librador.com>
2020-04-18 13:22:14 +0200

    Fix playfield timing

--- main.asm
+++ main.asm
@@ -183,15 +183,16 @@ game_frame:
 	sta WSYNC
 	stx COLUPF
 
-	lda avatar_sprite,y
-	sta GRP0
-
 	lda row_pf0l
 	sta PF0
 	lda row_pf1l
 	sta PF1
 	lda row_pf2l
 	sta PF2
+
+	lda avatar_sprite,y
+	sta GRP0
+	nop
 
 	lda row_pf0r
 	sta PF0
